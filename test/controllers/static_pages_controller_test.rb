0736e20ca7b74a8d7ad7ee8c16a4cf63375bf02e
Nathan Damtew <entenaterprise@gmail.com>
2016-12-08 07:13:27 +0300

    through to to

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,10 +6,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		@base_title="Ruby on Rails Tutorial Sample App"
 		
 	end
-  test "should get home" do
+  test "should get yenetta" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home| #{@base_title}"
+    assert_select "title", "Yenetta| #{@base_title}"
   end
 
 end
